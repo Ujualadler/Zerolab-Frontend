76f9b3aab4efd7e59760cd24bf5aa556a149e311
ujual <ujualk2020@gmail.com>
2024-09-03 09:27:14 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import Layout from "@/components/Layout";
-import Loader from "@/components/Loader";
-import MapShow from "@/components/Map";
-import Navbar from "@/components/Navbar";
 import dynamic from "next/dynamic";
+// import MapShow from "@/components/Map";
 import React from "react";
-
-const GoogleLoginBtn = dynamic(() => import("../components/GoogleLogin"), {
+const MapShow = dynamic(() => import("../components/Map"), {
   ssr: false,
 });
+
+// const GoogleLoginBtn = dynamic(() => import("../components/GoogleLogin"), {
+//   ssr: false,
+// });
 
 const Page: React.FC = () => {
   return (
