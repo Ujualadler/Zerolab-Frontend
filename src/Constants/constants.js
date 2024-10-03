3667e5d60951e6cbcf64e16d9684ab91a6b55a56
Vaishakh Prabhakaran <vaishakhp11@gmail.com>
2024-10-03 17:22:00 +0530

    changed baseUrl

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:4000";
-// export const baseURL = "https://api.felixartindia.com";
+// export const baseURL = "http://localhost:4000";
+export const baseURL = "https://api.felixartindia.com";
